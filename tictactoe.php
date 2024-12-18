74e1885b72b25e7be38b5ac4350f9dfda6f539cb
Azer-khadhraoui <azerronaldo2004@gmail.com>
2024-12-18 11:23:12 +0100

    Enhance The logic to display winner and draw conditions

--- tictactoe.php
+++ tictactoe.php
@@ -36,14 +36,16 @@ function check_winner($board) {
         if ($board[$combination[0]] !== '' &&
             $board[$combination[0]] === $board[$combination[1]] &&
             $board[$combination[1]] === $board[$combination[2]]) {
-            return $board[$combination[0]];
+            return $combination;
         }
     }
 
     return null;
 }
 
-$winner = check_winner($_SESSION['board']);
+$winner_combination = check_winner($_SESSION['board']);
+$winner = $winner_combination ? $_SESSION['board'][$winner_combination[0]] : null;
+$draw = !$winner && !in_array('', $_SESSION['board']);
 ?>
 
 <!DOCTYPE html>
@@ -73,27 +75,36 @@ $winner = check_winner($_SESSION['board']);
             height: 60px;
             text-align: center;
             font-size: 24px;
+            border: 1px solid #000;
         }
         button {
             width: 100%;
             height: 100%;
+            font-size: 24px;
+        }
+        .winner {
+            background-color: #90EE90;
         }
     </style>
 </head>
 <body>
     <div class="container">
         <h1>Tic-Tac-Toe</h1>
-        <?php if ($winner === null): ?>
+        <?php if ($winner === null && !$draw): ?>
             <h2>C'est le tour de : <?php echo $_SESSION['turn']; ?></h2>
+        <?php elseif ($winner !== null): ?>
+            <h2>Le gagnant est : <?php echo $winner; ?></h2>
+        <?php elseif ($draw): ?>
+            <h2>Match nul!</h2>
         <?php endif; ?>
-        <table border="1">
+        <table>
             <?php for ($i = 0; $i < 3; $i++): ?>
                 <tr>
                     <?php for ($j = 0; $j < 3; $j++): ?>
-                        <td>
+                        <td class="<?php echo in_array($i * 3 + $j, (array)$winner_combination) ? 'winner' : ''; ?>">
                             <?php
                             $index = $i * 3 + $j;
-                            if ($_SESSION['board'][$index] === '' && $winner === null): ?>
+                            if ($_SESSION['board'][$index] === '' && $winner === null && !$draw): ?>
                                 <form method="post">
                                     <input type="hidden" name="index" value="<?php echo $index; ?>">
                                     <button type="submit"></button>
@@ -110,11 +121,6 @@ $winner = check_winner($_SESSION['board']);
         <form method="post">
             <button type="submit" name="new_game">Play New Game</button>
         </form>
-
-        <?php if ($winner !== null): ?>
-            <h2>Le gagnant est : <?php echo $winner; ?></h2>
-            <?php session_destroy(); ?>
-        <?php endif; ?>
     </div>
 </body>
 </html>
